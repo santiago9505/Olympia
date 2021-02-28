7e1b3c4ebcfaaf7df76d9fb0274adc20a30f2d37
Santiago Guáqueta <santigeek@hotmail.com>
2021-02-28 17:47:47 -0500

    Adding signup section

---
+++ Olympia/src/pages/SignUp.js
@@ -0,0 +1,11 @@
+import CreateAccount from "../components/CreateAccount/CreateAccount.js";
+
+function SignUp() {
+  return (
+    <div className="grid grid-cols-12 relative">
+      <CreateAccount />
+    </div>
+  );
+}
+
+export default SignUp;
--- Olympia/src/routes/App.js
+++ Olympia/src/routes/App.js
@@ -1,19 +1,21 @@
 //React
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 //Components
-import Layout from '../components/Layout/Layout';
-import Home from '../pages/Home';
+import Layout from "../components/Layout/Layout";
+import Home from "../pages/Home";
+import SignUp from "../pages/SignUp";
 
 const App = () => (
   <BrowserRouter>
-  <Layout>
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/Home" component={Home}/>
-    </Switch>
-  </Layout>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Home" component={Home} />
+        <Route exact path="/Signup" component={SignUp} />
+      </Switch>
+    </Layout>
   </BrowserRouter>
 );
 
