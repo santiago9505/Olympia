bcb43ef90674dea6b3ae920e5458703cfa0704d4
Mitchell Mirano <mitchellmirano25@gmail.com>
2021-03-01 17:10:44 -0500

    creamos el modelo de los productos

--- productos/apps.py
+++ productos/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ProductosConfig(AppConfig):
     name = 'productos'
+    verbose_name='Productos'
--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    Name=models.CharField(max_length=50)
+
+    price=models.FloatField()
+
+    description=models.TextField()
+
+    stok=models.IntegerField()
+
+    img_url=models.ImageField(upload_to='productos/images')
+
+    product_choices=[
+        ('moda','Productos de moda'),
+        ('aseo', 'Productos de aseo personal')
+        ('manufactura','Productos de manufactura')
+    ]
+    tag_use=models.CharField(choices=product_choices)
+    
+    
+
+    def __str__(self):
+        return self.name
