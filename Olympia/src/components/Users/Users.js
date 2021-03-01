e8b8727eb1c684ce48aef7610231bc34e48e4c01
Santiago Guáqueta <santigeek@hotmail.com>
2021-02-28 22:45:43 -0500

    integration frontend/backend

---
+++ Olympia/src/components/Users/Users.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { API_URL } from "../../constants/index.js";
+
+const Users = () => {
+  state = {
+    pk: 0,
+    email: "",
+    password: "",
+    date_birth: "",
+    first_name: "",
+    last_name: "",
+    gender: "",
+  };
+
+  return <div></div>;
+};
+
+export default Users;
