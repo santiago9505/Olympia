30be70d39331fb41cc597459be540452bcf61a6e
Santiago Guáqueta <santigeek@hotmail.com>
2021-02-28 21:41:05 -0500

    Adding routing to profile

--- Olympia/src/components/Header/ProfileOptions.js
+++ Olympia/src/components/Header/ProfileOptions.js
@@ -1,32 +1,46 @@
 //React
-import React from 'react';
+import React from "react";
 
 //Libs
-import { FaGifts, FaHeart, FaHistory } from 'react-icons/fa';
-import { HiFire } from 'react-icons/hi';
-import { MdHome } from 'react-icons/md';
+import { FaGifts, FaHeart, FaHistory } from "react-icons/fa";
+import { HiFire } from "react-icons/hi";
+import { MdHome } from "react-icons/md";
 
 //Components
-import MenuItem from './MenuItem';
+import MenuItem from "./MenuItem";
 
 const ProfileOptions = () => {
   return (
     <>
       <ul className="grid grid-cols-1 grid-rows-5">
-        <MenuItem Icon={FaGifts} text="Tarjetas de regalo / cupones" type="profileOptions"/>
-        <MenuItem Icon={HiFire} text="Ofertas" type="profileOptions"/>
-        <MenuItem Icon={FaHeart} text="Favoritos" type="profileOptions"/>
-        <MenuItem Icon={FaHistory} text="Historial" type="profileOptions"/>
-        <MenuItem Icon={MdHome} text="Home" type="profileOptions"/>
+        <MenuItem
+          Icon={FaGifts}
+          text="Tarjetas de regalo / cupones"
+          type="profileOptions"
+        />
+        <MenuItem Icon={HiFire} text="Ofertas" type="profileOptions" />
+        <MenuItem Icon={FaHeart} text="Favoritos" type="profileOptions" />
+        <MenuItem Icon={FaHistory} text="Historial" type="profileOptions" />
+        <MenuItem Icon={MdHome} text="Home" type="profileOptions" />
       </ul>
-      <hr className="my-3 mx-auto w-3/4 border-t-2 border-gray-700"/>
+      <hr className="my-3 mx-auto w-3/4 border-t-2 border-gray-700" />
       <div className="grid grid-cols-2 grid-rows-1 gap-x-5">
         {/* Replace with Link tag */}
-        <a href="#" className="block outline-none text-center rounded-full bg-white p-1">Login</a>
-        <a href="#" className="block outline-none text-center rounded-full bg-white p-1">Register</a>
+        <a
+          href="/login"
+          className="block outline-none text-center rounded-full bg-white p-1"
+        >
+          Login
+        </a>
+        <a
+          href="/SignUp"
+          className="block outline-none text-center rounded-full bg-white p-1"
+        >
+          Register
+        </a>
       </div>
     </>
   );
-}
+};
 
 export default ProfileOptions;
