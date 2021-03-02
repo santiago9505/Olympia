5a051df6c3d18f695fde05491282b02d04d15601
Santiago Guáqueta <santigeek@hotmail.com>
2021-03-02 18:59:49 -0500

    Adding md responsive to Login

--- Olympia/src/pages/Login/Login.js
+++ Olympia/src/pages/Login/Login.js
@@ -1,11 +1,11 @@
 //React
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 //Libs
-import { FaFacebookSquare, FaInstagram, FaLinkedin } from 'react-icons/fa';
-import { MdEmail } from 'react-icons/md';
-import { CgImage } from 'react-icons/cg';
+import { FaFacebookSquare, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { CgImage } from "react-icons/cg";
 
 const Login = () => {
   return (
@@ -13,67 +13,84 @@ const Login = () => {
       <div className="text-center text-2xl">
         <h1>
           Log into
-          <br/>
+          <br />
           "Name E-Commerce"
         </h1>
       </div>
       <div>
-        <form className="flex flex-col mb-2">
-          <label htmlFor="email">Account</label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            placeholder="Email address or member ID"
-            className="text-sm py-1 px-2 bg-gray-400 placeholder-gray-600 rounded-full mb-3"
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Password"
-            className="text-sm py-1 px-2 bg-gray-400 placeholder-gray-600 rounded-full mb-3"
-          />
-          <label className="mb-3">
+        <form className="flex flex-col mb-2 items-center md:grid md:grid-cols-1 md:justify-items-center md:gap-4 md:mt-8">
+          <div className="md:flex md:gap-4">
+            <label className="flex justify-center" htmlFor="email">
+              Account
+            </label>
             <input
-              type="checkbox"
-              name="staySigned"
-              className="mr-2"
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Email address or member ID"
+              className="flex text-sm py-1 px-2 bg-gray-400 placeholder-gray-600 rounded-full mb-3 w-80"
             />
+          </div>
+          <div className="md:flex md:gap-3">
+            <label className="flex justify-center" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              className="flex text-sm py-1 px-2 bg-gray-400 placeholder-gray-600 rounded-full mb-3 w-80"
+            />
+          </div>
+
+          <label className="mb-3">
+            <input type="checkbox" name="staySigned" className="mr-2" />
             Stay signed in
           </label>
           <input
             type="submit"
             value="Sign in"
-            className="w-2/5 py-0.5 m-auto mb-1 rounded-full text-lg border-2 border-gray-400 bg-white active:bg-gray-400"
+            className="w-40 py-0.5 m-auto mb-1 rounded-full text-lg border-2 border-gray-400 bg-white active:bg-gray-400 md:w-52"
           />
-          <p className="text-xs text-center underline">I've forgotten my password</p>
+          <p className="text-xs text-center underline">
+            I've forgotten my password
+          </p>
         </form>
         <div className="flex justify-between">
-          <hr className="my-3 flex-grow border-t-2 border-gray-400"/>
+          <hr className="my-3 flex-grow border-t-2 border-gray-400" />
           <span className="w-8 text-center">o</span>
-          <hr className="my-3 flex-grow border-t-2 border-gray-400"/>
+          <hr className="my-3 flex-grow border-t-2 border-gray-400" />
         </div>
       </div>
       <div className="text-center">
         <h2 className="text-xl mb-2">Sign in with</h2>
-        <div className="flex justify-between text-3xl w-2/4 m-auto mb-5">
-          <span><FaFacebookSquare/></span>
-          <span><FaInstagram/></span>
-          <span><MdEmail/></span>
-          <span><FaLinkedin/></span>
+        <div className="flex justify-center gap-2 text-3xl w-2/4 m-auto mb-5 md:gap-4 md:mt-3">
+          <span>
+            <FaFacebookSquare />
+          </span>
+          <span>
+            <FaInstagram />
+          </span>
+          <span>
+            <MdEmail />
+          </span>
+          <span>
+            <FaLinkedin />
+          </span>
         </div>
         <p className="text-xs">
           New to "name e-commerce"? &nbsp;
-          <Link to="/signup" className="underline">Create an account</Link>
+          <Link to="/signup" className="underline">
+            Create an account
+          </Link>
         </p>
       </div>
       <div className="flex justify-center text-9xl">
-        <CgImage/>
+        <CgImage />
       </div>
     </div>
   );
-}
+};
 
 export default Login;
