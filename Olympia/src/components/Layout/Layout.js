d71c2c4c3634a21a02d3f11e868430aa39674cc4
Jose Daniel Molina <josedanielmolina323@gmail.com>
2021-03-03 15:17:14 -0500

    Float options added, this component have some options about language and currency etc

--- Olympia/src/components/Layout/Layout.js
+++ Olympia/src/components/Layout/Layout.js
@@ -4,12 +4,14 @@ import React from 'react';
 //Static Components
 import Navbar from '../Navbar/Navbar';
 import Header from '../Header/Header';
+import FloatOptions from '../FloatOptions/FloatOptions';
 
 const Layout = ({ children }) => (
   <div className="relative">
     <Navbar/>
     { children }
     <Header/>
+    <FloatOptions/>
   </div>
 );
 
