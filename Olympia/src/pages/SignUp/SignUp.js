80840740593313ee8dd612c4114dbe9651e8e370
David-Acer <codingdavid9@outlook.com>
2021-03-05 12:42:18 -0500

    Estilos en pantalla SignUp

--- Olympia/src/pages/SignUp/SignUp.js
+++ Olympia/src/pages/SignUp/SignUp.js
@@ -2,7 +2,7 @@ import CreateAccount from "../../components/CreateAccount/CreateAccount";
 
 function SignUp() {
   return (
-    <div className="grid grid-cols-12 relative">
+    <div className="grid grid-cols-12 relative bg-white">
       <CreateAccount />
     </div>
   );
